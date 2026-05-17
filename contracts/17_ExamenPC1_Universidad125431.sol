d28d31880ff28a76ba00af4b2fbd0ab51662845d
RamosCVania <vramosc2@upao.edu.pe>
2026-05-16 20:17:25 -0500

    Parte 3

--- contracts/17_ExamenPC1_Universidad125431.sol
+++ contracts/17_ExamenPC1_Universidad125431.sol
@@ -9,14 +9,19 @@ contract Universidad125431 {
         uint id;
         string nombre;
         uint ciclo;
+         bool estado;
     }
 
     Alumno[] public alumnos;
 
     address public dirContrato125431;
 
+    modifier ejecutarPor125431() {
+    console.log("Ejecutado por: 125431 - Vania Melissa Ramos Cotrina");
+    _;
+    }
+
     constructor() {
-        console.log("Ejecutado por: 125431 - Vania Melissa Ramos Cotrina");
         dirContrato125431 = address(this);
     }
 
@@ -24,15 +29,20 @@ contract Universidad125431 {
         uint _id,
         string memory _nombre,
         uint _ciclo
-    ) public {
+    ) public ejecutarPor125431 {
+
+        for(uint i = 0; i < alumnos.length; i++) {
+            require(alumnos[i].id != _id, "Alumno con ese ID ya existe");
+        }
+
+        require(bytes(_nombre).length > 0, "Nombre vacio");
 
         alumnos.push(
-            Alumno(_id, _nombre, _ciclo)
+            Alumno(_id, _nombre, _ciclo, true)
         );
     }
 
     function contarElementos125431() public view returns(uint) {
-        console.log("Ejecutado por: 125431 - Vania Melissa Ramos Cotrina");
         return alumnos.length;
     }
 
