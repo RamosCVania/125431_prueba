648a54d94eaa6b828fbc4ba214ad61321f02a913
RamosCVania <vramosc2@upao.edu.pe>
2026-05-16 20:40:21 -0500

    Parte 5

--- contracts/17_ExamenPC1_Universidad125431.sol
+++ contracts/17_ExamenPC1_Universidad125431.sol
@@ -9,12 +9,14 @@ contract Universidad125431 {
         uint id;
         string nombre;
         uint ciclo;
-         bool estado;
+        bool estado;
     }
 
-    Alumno[] public alumnos;
+    mapping(uint => Alumno) public alumnos;
 
     address public dirContrato125431;
+
+    uint256 public cantidad125431;
 
     modifier ejecutarPor125431() {
     console.log("Ejecutado por: 125431 - Vania Melissa Ramos Cotrina");
@@ -31,34 +33,32 @@ contract Universidad125431 {
         uint _ciclo
     ) public ejecutarPor125431 {
 
-        for(uint i = 0; i < alumnos.length; i++) {
-            require(alumnos[i].id != _id, "Alumno con ese ID ya existe");
-        }
+        require(alumnos[_id].id != _id, "Alumno con ese ID ya existe");
 
         require(bytes(_nombre).length > 0, "Nombre vacio");
 
-        alumnos.push(
-            Alumno(_id, _nombre, _ciclo, true)
+        alumnos[_id] = Alumno(
+            _id,
+            _nombre,
+            _ciclo,
+            true
         );
+
+        cantidad125431++;
     }
 
     function contarElementos125431() public view ejecutarPor125431 returns(uint) {
-        return alumnos.length;
+        return cantidad125431;
     }
 
-    function inactivarElemento125431(uint _posicion)
-    public
-    ejecutarPor125431
+    function inactivarElemento125431(uint _id) public ejecutarPor125431
     {
-        alumnos[_posicion].estado = false;
+        alumnos[_id].estado = false;
     }
 
-    function pintarElementosActivos125431()
-    public
-    view
-    ejecutarPor125431
+    function pintarElementosActivos125431() public view ejecutarPor125431
     {
-        for(uint i = 0; i < alumnos.length; i++) {
+        for(uint i = 1; i <= cantidad125431; i++) {
 
             if(alumnos[i].estado == true) {
 
